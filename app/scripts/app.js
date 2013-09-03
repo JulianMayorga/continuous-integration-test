5318d61f5d1461894aa12aea9b63fb7d70d02338
JulianMayorga <mayorga.julian@gmail.com>
2013-09-03 19:36:22 -0300

    Esqueleto yeoman de angular

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('continuousDeploymentTestApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
