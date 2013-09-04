3b60107d3f937c25dc6c9a0b48924cd85a462800
Juli <mayorga.julian@gmail.com>
2013-09-04 14:45:52 +0000

    adding the yeoman build files to gh-pages [ci skip]

---
+++ scripts/abb979cf.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("continuousDeploymentTestApp",[]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("continuousDeploymentTestApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Nave","Karma"]}]);
--- scripts/aea6289d.scripts.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("continuousDeploymentTestApp",[]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("continuousDeploymentTestApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
